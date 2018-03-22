fd2a855a029e1c1b0b1d00a1c07762468e50361c
Aggrathon <antonbjo@gmail.com>
2018-03-22 20:53:49 +0200

    Adjust parameters

--- models/skip_gan.py
+++ models/skip_gan.py
@@ -11,7 +11,7 @@ CODE_SIZE = 400
 LEARNING_RATE = 2e-5
 
 def _generator(data, reuse=False, training=True):
-    with tf.variable_scope('generator', reuse=reuse):
+    with tf.variable_scope('generator', reuse=reuse) as scope:
         prev_layer = tf.reshape(tf.layers.dense(data, 8*11*8, activation=tf.nn.relu), (BATCH_SIZE, 8, 11, 8), 'layer0')
         with tf.variable_scope('layer1'):
             prev_layer = tf.layers.conv2d(prev_layer, 128, 3, 1, 'same', activation=tf.nn.relu)
@@ -42,10 +42,13 @@ def _generator(data, reuse=False, training=True):
             skip = tf.reshape(tf.layers.dense(data, 130*178, activation=tf.nn.relu), (BATCH_SIZE, 130, 178, 1))
             prev_layer = prev_layer + tf.layers.conv2d(skip, 64, 3, 1, 'valid')
             prev_layer = tf.layers.conv2d(prev_layer, 3, 5, 1, 'same', activation=tf.nn.tanh)
-        return prev_layer
+        if reuse:
+            return prev_layer
+        else:
+            return prev_layer, scope.trainable_variables()
 
 def _discriminator(data, reuse=False, training=True):
-    with tf.variable_scope('discriminator', reuse=reuse):
+    with tf.variable_scope('discriminator', reuse=reuse) as scope:
         with tf.variable_scope('layer0'):
             prev_layer = tf.layers.conv2d(data, 32, 3, 1, 'same', activation=tf.nn.relu)
             prev_layer = tf.layers.conv2d(prev_layer, 32, 3, 2, 'same', activation=tf.nn.relu)
@@ -78,7 +81,10 @@ def _discriminator(data, reuse=False, training=True):
             prev_layer = tf.layers.dense(prev_layer, 256, activation=tf.nn.relu)
             prev_layer = tf.layers.dropout(prev_layer, 0.3, training=training)
             prev_layer = tf.layers.dense(prev_layer, 1, name='logits')
-        return prev_layer
+        if reuse:
+            return prev_layer
+        else:
+            return prev_layer, scope.trainable_variables()
 
 class SkipGanGenerator(Generator):
     """
@@ -90,8 +96,8 @@ class SkipGanGenerator(Generator):
         with self.scope:
             self.seed = tf.random_uniform((BATCH_SIZE, CODE_SIZE), -1.0, 1.0)
             self.real_image = get_art_only_data(batch_size=BATCH_SIZE)
-            self.fake_image = _generator(self.seed, False, training)
-            self.real_disc = _discriminator(self.real_image, False, training)
+            self.fake_image, gen_vars = _generator(self.seed, False, training)
+            self.real_disc, disc_vars = _discriminator(self.real_image, False, training)
             self.fake_disc = _discriminator(self.fake_image, True, training)
             #gp
             x_hat = self.real_image + tf.random_uniform([BATCH_SIZE, 1, 1, 1], 0.0, 1.0) * (self.fake_image - self.real_image)
@@ -100,34 +106,38 @@ class SkipGanGenerator(Generator):
             slope = tf.sqrt(tf.reduce_sum(tf.square(gradients), [1, 2, 3]))
             self.gradient_penalty = tf.reduce_mean(tf.square(slope-1.0), name='gradient_penalty')
             #losses
-            self.loss_g = -tf.reduce_mean(self.fake_disc, name='loss_g')
-            self.loss_d = tf.losses.compute_weighted_loss(
-                [tf.reduce_mean(self.fake_disc), tf.reduce_mean(self.real_disc), self.gradient_penalty],
-                [1.0, -1.0, 10.0]
-            )
-            adam_d = tf.train.AdamOptimizer(LEARNING_RATE, 0.0, 0.9, name='AdamD')
-            adam_g = tf.train.AdamOptimizer(LEARNING_RATE, 0.0, 0.9, name='AdamG')
-            with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
-                self.trainer_d = adam_d.minimize(
-                    self.loss_d,
-                    var_list=tf.trainable_variables('discriminator'),
-                    name='train_d'
-                )
-                trainer_g = adam_g.minimize(
-                    self.loss_g,
-                    global_step=self.global_step,
-                    var_list=tf.trainable_variables('generator'),
-                    name='train_g'
-                )
-            #summary
+            rmf = tf.reduce_mean(self.fake_disc)
+            rmr = tf.reduce_mean(self.real_disc)
+            diff = tf.maximum(0.0, rmr - rmf)
+            self.loss_g = tf.negative(rmf, name='loss_g')
+            self.loss_d = tf.losses.compute_weighted_loss( [rmf, rmr, self.gradient_penalty], [1.0, -1.0, 10.0])
+            #meta
+            self.distance = tf.get_variable('distance', [], tf.float32, trainable=False, initializer=tf.initializers.ones)
             self.measure = tf.get_variable('measure', [], tf.float32, trainable=False, initializer=tf.initializers.zeros)
             measure = tf.assign(self.measure, 0.95*self.measure-0.05*self.loss_d)
+            distance = tf.assign(self.distance, 0.95*self.distance + 0.05*diff)
+            #training
+            adam = tf.train.AdamOptimizer(LEARNING_RATE, 0.0, 0.9, name='Adam')
+            with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+                self.trainer_d = adam.minimize(
+                    self.loss_d,
+                    var_list=disc_vars,
+                    name='train_d'
+                )
+                trainer_g = adam.minimize(
+                    self.loss_g,
+                    global_step=self.global_step,
+                    var_list=gen_vars,
+                    name='train_g'
+                )
+            self.trainer_g = tf.group(self.trainer_d, trainer_g, measure, distance)
+            #summary
             tf.summary.scalar("Measure", measure)
+            tf.summary.scalar("Distance", distance)
             tf.summary.scalar("GradientPenalty", self.gradient_penalty)
             tf.summary.scalar("LossD", self.loss_d)
             tf.summary.scalar("LossG", self.loss_g)
             tf.summary.image("Generated", self.fake_image, 4)
-            self.trainer_g = tf.group(trainer_g, measure)
 
     def train_step(self, summary=False, session=None):
         """
@@ -135,7 +145,7 @@ class SkipGanGenerator(Generator):
         """
         if session is None:
             session = self.session
-        for _ in range(8):
+        for _ in range(10):
             session.run(self.trainer_d)
         if summary:
             _, smry, step, res = session.run([self.trainer_g, self.summary, self.global_step, self.measure])
